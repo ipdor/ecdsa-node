5e2dfee46db463f6de1576a854fea4d1686cab54
Nate <nate.yoran@gmail.com>
2025-12-12 13:52:23 +0900

    Complete basic requirements

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,5 +1,27 @@
 import { useState } from "react";
 import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1"; //导出方式是 named export，不是 default export。不能省略* as 和花括号
+import { toHex, utf8ToBytes } from "ethereum-cryptography/utils"; 
+import { sha256 } from "ethereum-cryptography/sha256";
+
+const privateKey = {
+  "0xec0f1fcc0f643b08030b": "25a334b77bc508cd5897f691e89bdf3f8b5e3f2a69f42ea6bdfb439834ba56bf",
+  "0xb9b79aee6d0d6f0d51ad": "302f3e87e28f4c17ed8c440b0fb730780a1e848dff04a1dfd0930988c7908d0a",
+  "0x2f70dd80bea73f15b571": "2003a4bf3b29458b7aa747221d05930e1cfe6c05d34f202aab18bba202dcd5ab"
+}
+
+async function sign(transaction){
+  // 使用replacer_array确保序列化后Object内部键值对顺序一致
+  const replacer_array = ["sender", "amount", "recipient", "timeStamp"];
+  // either a hex string, or Uint8Array
+  const msgHash = sha256(utf8ToBytes(JSON.stringify(transaction, replacer_array)));
+  // 注意可恢复的签名返回两个值
+  const [signature, recovery] = await secp.sign(msgHash, privateKey[transaction.sender], { recovered:true });
+  return {
+    signature: toHex(signature), 
+    recovery
+  };
+}
 
 function Transfer({ address, setBalance }) {
   const [sendAmount, setSendAmount] = useState("");
@@ -11,12 +33,18 @@ function Transfer({ address, setBalance }) {
     evt.preventDefault();
 
     try {
-      const {
-        data: { balance },
-      } = await server.post(`send`, {
+      const transaction = {
         sender: address,
         amount: parseInt(sendAmount),
         recipient,
+        timeStamp: Date.now(),
+      }
+      const signature = await sign(transaction);
+      const {
+        data: { balance },
+      } = await server.post(`send`, {
+        transaction,
+        signature,
       });
       setBalance(balance);
     } catch (ex) {
--- server/index.js
+++ server/index.js
@@ -2,34 +2,72 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const secp = require("ethereum-cryptography/secp256k1");
+const { toHex,utf8ToBytes } = require("ethereum-cryptography/utils");
+const { sha256 } = require("ethereum-cryptography/sha256");
+
+const TIMEOUT = 300000 //5分钟超时时间
+
+function generateKey(){
+  for(let i = 0; i < n; i++){
+    const privateKey = secp.utils.randomPrivateKey();
+    const publicKey = secp.getPublicKey(privateKey);
+
+    console.log("Private Key:", toHex(privateKey));
+    console.log("Public Key:", toHex(publicKey));
+    console.log("id:", "0x" + toHex(publicKey).slice(-20));
+  }
+}
 
 app.use(cors());
 app.use(express.json());
 
+
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0xec0f1fcc0f643b08030b": 100,
+  "0xb9b79aee6d0d6f0d51ad": 50,
+  "0x2f70dd80bea73f15b571": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
+  //console.log(req.url);
   const { address } = req.params;
   const balance = balances[address] || 0;
   res.send({ balance });
 });
 
-app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+app.post("/send", async (req, res) => {
+  try {
+    const { transaction, signature } = req.body;
+    const { sender, amount, recipient, timeStamp } = transaction;
+    
+    const recoverPK = async (transaction, sig) => {
+      const replacer_array = ["sender", "amount", "recipient", "timeStamp"];
+      const msgHash = toHex(sha256(utf8ToBytes(JSON.stringify(transaction, replacer_array))));
+      const pk = await secp.recoverPublicKey(msgHash, sig.signature, sig.recovery);
+      return toHex(pk);
+    }
+    
+    const pk = await recoverPK(transaction, signature);
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+    if(pk.slice(-20) != sender.slice(2))
+      res.status(400).send({ message: "Invalid sender: signature mismatch!"});
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+    if((Date.now() - timeStamp) > TIMEOUT)
+      res.status(400).send({ message: "Transaction expired!"});
+    
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
+
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
+  } catch (error) {
+    console.log(error.message);
   }
 });
 
